bb47618eb9d5b61f97590035d2440b6d8c0282bd
Vladis <vladisrud@gmail.com>
2022-07-08 18:38:32 +0300

    Initial Commit

---
+++ LearnUIKitProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnUIKitProgrammatically
+//
+//  Created by Влад Руденко on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
