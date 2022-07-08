6c9b399d3bbc186ec143c766d26f5ca865021f26
Vladis <vladisrud@gmail.com>
2022-07-08 21:25:13 +0300

    Learn, learn and repeat

--- LearnUIKitProgrammatically/ViewController.swift
+++ LearnUIKitProgrammatically/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let firstView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = true
+        view.sizeToFit()
+        return view
+    }()
+    
+    let firstLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TEST"
+        label.font = .systemFont(ofSize: 15.5)
+        label.textColor = .white
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.addSubview(firstView)
+        
+        
     }
-
 
 }
 
